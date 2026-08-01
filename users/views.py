fa032d175921bf4efddcefedb0143427af69d5ff
DevAyushKumar <blaze.main.id@gmail.com>
2026-08-01 21:43:13 +0530

    Login changes

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -20,3 +21,7 @@ def login(response):
 
 def logout(response):
     return render(response, 'users/logout.html')
+
+@login_required
+def profile(response):
+    return render(response, 'users/profile.html')
